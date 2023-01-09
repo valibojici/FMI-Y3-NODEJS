7cf7221317b8a41d53512a8431a512cbc1c725dc
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-01-09 19:14:22 +0200

    add course and grade models & migrations

---
+++ models/grade.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Grade extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Grade.init({
+    grade: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Grade',
+  });
+  return Grade;
+};
