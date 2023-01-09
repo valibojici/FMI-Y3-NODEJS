3825f6ee90eaa0198d1ec093f416187dfcc58d52
Edi Podaru <edipodaru@gmail.com>
2023-01-09 19:15:47 +0200

    Add create professor mutation

---
+++ graphql/resolvers/createProfessorResolver.js
@@ -0,0 +1,14 @@
+const models = require("../../models");
+module.exports = async (source, { firstName, lastName, email }, { tokenPayload }) => {
+  if(!tokenPayload) {
+    return null;
+  }
+
+  const user = await models.Professor.create({
+    firstName,
+    lastName,
+    email,
+  });
+
+  return user;
+}
