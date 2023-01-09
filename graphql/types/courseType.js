800249fb135a65eab5b49cf4d88e7d7c61d232d3
Edi Podaru <edipodaru@gmail.com>
2023-01-09 20:28:21 +0200

    Modify courseType to avoid circular imports

--- graphql/types/courseType.js
+++ graphql/types/courseType.js
@@ -5,28 +5,30 @@ const {
     GraphQLList,
 } = require('graphql');
 
-const professorType = require("./professorType")
-const studentType = require("./studentType")
-
 const courseType = new GraphQLObjectType({
     name: 'Course',
-    fields: {
-        id: {
-            type: GraphQLID,
-        },
-        name: {
-            type: GraphQLString,
-        },
-        professor: {
-            type: professorType,
-            resolve: (course) => {return course.getProfessor()}
-        },
-        students: {
-            type: new GraphQLList(studentType),
-            resolve: async (course) => {
-                return course.getStudents();
-            }
-        },
+    fields: () => {
+        const professorType = require("./professorType")
+        const studentType = require("./studentType")
+        
+        return ({
+            id: {
+                type: GraphQLID,
+            },
+            name: {
+                type: GraphQLString,
+            },
+            professor: {
+                type: professorType,
+                resolve: (course) => { return course.getProfessor() }
+            },
+            students: {
+                type: new GraphQLList(studentType),
+                resolve: async (course) => {
+                    return course.getStudents();
+                }
+            },
+        })
     }
 });
 
