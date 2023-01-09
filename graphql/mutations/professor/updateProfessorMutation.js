c542353fbbcb7276cd8f2bd3d7a24bd7aad5de59
Edi Podaru <edipodaru@gmail.com>
2023-01-09 19:34:41 +0200

    Add update professor mutation

---
+++ graphql/mutations/professor/updateProfessorMutation.js
@@ -0,0 +1,25 @@
+const {
+    GraphQLNonNull,
+    GraphQLString, GraphQLID
+  } = require("graphql");
+  const professorType = require("../types/professorType");
+  const updateProfessorResolver = require('../resolvers/updateProfessorResolver');
+
+  module.exports = {
+    type: professorType,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+      },
+      firstName: {
+        type: GraphQLString,
+      },
+      lastName: {
+        type: GraphQLString,
+      },
+      email: {
+        type: GraphQLString,
+      }
+    },
+    resolve: updateProfessorResolver,
+  }
---
+++ graphql/resolvers/professor/updateProfessorResolver.js
@@ -0,0 +1,24 @@
+const models = require("../../models");
+module.exports = async (source, args, { tokenPayload }) => {
+  const {
+    id,
+    firstName,
+    lastName,
+    email,
+  } = args;
+  if(!tokenPayload) {
+    return null;
+  }
+
+  await models.Professor.update({
+    firstName,
+    lastName,
+    email
+  }, {
+    where: {
+      id,
+    }
+  });
+
+  return models.Professor.findByPk(id);
+}
