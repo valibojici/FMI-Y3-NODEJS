54146068f387f9c8e9a3f61a9e0b990daa78dabe
Edi Podaru <edipodaru@gmail.com>
2023-01-10 17:44:12 +0200

    Update studentType

--- graphql/types/studentType.js
+++ graphql/types/studentType.js
@@ -1,22 +1,40 @@
-const { 
+const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
+    GraphQLList,
 } = require('graphql');
 
 
 const studentType = new GraphQLObjectType({
     name: 'Student',
-    fields: {
-        id: {
-            type: GraphQLID,
-        },
-        firstName: {
-            type: GraphQLString,
-        },
-        lastName: {
-            type: GraphQLString,
-        }
+    fields: () => {
+        const courseType = require("./courseType");
+        const gradeType = require("./gradeType");
+
+        return ({
+            id: {
+                type: GraphQLID,
+            },
+            firstName: {
+                type: GraphQLString,
+            },
+            lastName: {
+                type: GraphQLString,
+            },
+            courses: {
+                type: new GraphQLList(courseType),
+                resolve: async (student) => {
+                    return student.getCourses();
+                }
+            },
+            grades: {
+                type: new GraphQLList(gradeType),
+                resolve: async (student) => {
+                    return student.getGrades();
+                }
+            }
+        })
     }
 });
 
